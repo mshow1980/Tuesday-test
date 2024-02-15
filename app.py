e67eaec9b5030bc080d03b05e6e3d24732ad9188
Micheal Soyemi <mshow1980@aol.com>
2024-02-14 19:40:01 -0700

    updated app

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Made Easy"
+    return "HMMMMM!!!! MICHEAL, THE NEW DEVOP ENGINEER IN TOWN"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
